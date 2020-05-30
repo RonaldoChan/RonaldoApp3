0db160f76629c52352ee8649323541c97b3c018f
RonaldoChan <ronaldo973359101@gmail.com>
2020-05-30 14:51:41 -0500

    navigate

---
+++ src/screen/home.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useLinkProps } from "@react-navigation/native";
+
+
+export default function App(props) {
+    return (
+        <View style={styles.container}>
+
+            <TouchableOpacity
+                onPress={() => { props.navigation.navigate("Login") }}
+                style={styles.vTexto}>
+                <Text style={styles.texto}>
+                    INGRESAR
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.vTexto}>
+                <Text style={styles.texto}>
+                    REGISTRARSE
+                </Text>
+            </TouchableOpacity>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#222f3e'
+    },
+    vImagen: {
+        height: 200,
+        width: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+
+
+    },
+    vTexto: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        marginBottom: 10,
+        borderWidth: 1,
+        height: 50,
+        width: 230,
+        borderRadius: 15,
+        borderColor: '#ff6b6b',
+        backgroundColor: '#ff6b6b',
+
+    },
+    texto: {
+        color: '#222f3e',
+        fontSize: 25,
+        fontWeight: 'bold'
+    }
+});
---
+++ src/screen/login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+
+
+export function login() {
+    return (
+        <View>
+            <Text>
+                hola
+            </Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
+})
+
+
+
